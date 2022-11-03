8b7e6436e687773f49b7283e3c348f03aba9265c
Juana Paiva <juanapaiva2019@gmail.com>
2022-11-03 12:51:37 -0300

    chore: project creation

---
+++ src/main/java/com/api/orangeevolution/OrangeEvolutionApplication.java
@@ -0,0 +1,13 @@
+package com.api.orangeevolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeEvolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangeEvolutionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/orangeevolution/OrangeEvolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.orangeevolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangeEvolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
