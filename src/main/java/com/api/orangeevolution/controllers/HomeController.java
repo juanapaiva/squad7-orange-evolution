a136aaf2e55db6cc7ecaa4538f3f037657124324
Juana Paiva <juanapaiva2019@gmail.com>
2022-11-08 23:03:02 -0300

    feat: user entity

--- src/main/java/com/api/orangeevolution/controllers/HomeController.java
+++ src/main/java/com/api/orangeevolution/controllers/HomeController.java
@@ -1,13 +1,15 @@
 package com.api.orangeevolution.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
 	@RequestMapping("/")
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("nome", "Fulano");
 		return "home/index";
 	}
 }
---
+++ src/main/java/com/api/orangeevolution/entities/User.java
@@ -0,0 +1,105 @@
+package com.api.orangeevolution.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Users")
+public class User implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	private String email;
+	private String password;
+	private String photo;
+	private String is_admin;
+	
+	public User() {}
+
+	public User(Integer id, String name, String email, String password, String photo, String is_admin) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.photo = photo;
+		this.is_admin = is_admin;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+
+	public String getIs_admin() {
+		return is_admin;
+	}
+
+	public void setIs_admin(String is_admin) {
+		this.is_admin = is_admin;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
