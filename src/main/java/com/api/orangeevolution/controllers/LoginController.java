4699d1f288defc64a8b47b788887511b8dde368c
Juana Paiva <juanapaiva2019@gmail.com>
2022-11-14 00:41:41 -0300

    feat: user registration

--- src/main/java/com/api/orangeevolution/controllers/LoginController.java
+++ src/main/java/com/api/orangeevolution/controllers/LoginController.java
@@ -1,13 +1,19 @@
 package com.api.orangeevolution.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.api.orangeevolution.entities.User;
+import com.api.orangeevolution.repositories.UserRepository;
 
 @Controller
 public class LoginController {
+	
+	@Autowired
+	private UserRepository userRepository;
 
 	@GetMapping("/")
 	public ModelAndView login() {
@@ -21,6 +27,15 @@ public class LoginController {
 		ModelAndView mv = new ModelAndView();
 		mv.addObject("user", new User());
 		mv.setViewName("cadastro");
+		return mv;
+	}
+	
+	@PostMapping("saveUser")
+	public ModelAndView signIn(User user) {
+		ModelAndView mv = new ModelAndView();
+		user.setIs_admin("N");
+		userRepository.save(user);
+		mv.setViewName("redirect:/dashboard_usuario");
 		return mv;
 	}
 }
