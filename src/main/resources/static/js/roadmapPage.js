c7e554aaeff225d23b7854c039afb2a25d86c022
Juana Paiva <juanapaiva2019@gmail.com>
2022-11-15 18:11:37 -0300

    roadmapPage js

--- src/main/resources/static/js/roadmapPage.js
+++ src/main/resources/static/js/roadmapPage.js
@@ -4,6 +4,8 @@ const urlRoadmap = `http://localhost:8080/roadmaps/${roadmapId}`;
 
 const roadmapDescription = document.querySelector('.roadmapDescription');
 const contentCategories = document.querySelector('.contentCategories');
+const contentBox = document.querySelector('.contentBox');
+const categoryName = document.querySelector('.categoryName');
 
 let categories = []
 
@@ -42,6 +44,117 @@ const addItemIntoRoadmap = async () => {
     </div>
   `).join("");
   contentCategories.innerHTML += contentCategoriesTemplate;
+
+  const contentBoxTemplate = roadmap.contents.map((item) => `
+  <div class="col-12 categoryName"></div>
+
+  <div class="d-flex flex-wrap col-12 justify-content-end">  
+    <p class="small-copy me-2">Filtrar</p>        
+    <div>
+      <img src="../images/filter-icon.svg" alt="">
+    </div>
+  </div>
+
+  <div class="d-flex flex-wrap col-12 justify-content-end text-center mt-2 contentTypes">          
+    <div class="tag-dark me-2">
+      <p>TYPE</p>
+    </div>          
+    <div class="tag-dark me-2">
+      <p>TYPE</p>
+    </div>          
+    <div class="tag-dark me-2">
+      <p>TYPE</p>
+    </div>          
+    <div class="tag-dark me-2">
+      <p>TYPE</p>
+    </div>          
+    <div class="tag-dark">
+      <p>TYPE</p>
+    </div>
+  </div>
+
+  <div class="col-12 p-4 mt-2 mb-2 bg-black-evolution contentInfos">
+    <div>
+      <p class="body-bold text-white">TITLE</p>
+    </div>
+    <div class="d-md-flex align-items-baseline mt-2">          
+      <p class="body-bold text-white me-md-3">OWNER</p>
+      <p class="body-copy me-md-3">DURATION</p>          
+      <div class="tag-white text-center me-md-3">
+        <p>TYPE</p>
+      </div>
+    </div>
+
+    <div class="mt-3">
+      <p class="body-copy text-white">SUBJECT</p>
+    </div>
+    <div class="d-md-flex justify-content-end text-center mt-3">
+      <div class="me-0 me-md-2">
+        <button type="button" class="btn-evolution-outline-purple btn-evolution-text px-md-5">ACESSAR</button> <!-- LINK -->
+      </div>
+      <div class="mt-3 mt-md-0">
+        <button type="button" class="btn-evolution-teal btn-evolution-text px-md-5" data-bs-toggle="modal" data-bs-target="#exampleModal">CONCLUÍDO</button>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="col-12 p-4 mb-2 bg-black-evolution">
+    <div>
+      <p class="body-bold text-white">TITLE</p>
+    </div>
+    <div class="d-md-flex align-items-baseline mt-2">          
+      <p class="body-bold text-white me-md-3">OWNER</p>
+      <p class="body-copy me-md-3">DURATION</p>          
+      <div class="tag-white text-center me-md-3">
+        <p>TYPE</p>
+      </div>
+    </div>
+
+    <div class="mt-3">
+        <p class="body-copy text-white">SUBJECT</p>
+    </div> 
+    <div class="d-md-flex justify-content-end text-center mt-3">
+      <div class="me-0 me-md-2">
+        <button type="button" class="btn-evolution-outline-purple btn-evolution-text px-md-5">ACESSAR</button>
+      </div>
+      <div class="mt-3 mt-md-0">
+        <button type="button" class="btn-evolution-teal btn-evolution-text px-md-5" data-bs-toggle="modal" data-bs-target="#exampleModal">CONCLUÍDO</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-12 p-4 mb-2 bg-black-evolution">
+    <div>
+      <p class="body-bold text-white">TITLE</p>
+    </div>
+    <div class="d-md-flex align-items-baseline mt-2">          
+      <p class="body-bold text-white me-md-3">OWNER</p>
+      <p class="body-copy me-md-3">DURATION</p>          
+      <div class="tag-white text-center me-md-3">
+        <p>TYPE</p>
+      </div>
+    </div>
+
+    <div class="mt-3">
+      <p class="body-copy text-white">SUBJECT</p>
+    </div> 
+    <div class="d-md-flex justify-content-end text-center mt-3">
+      <div class="me-0 me-md-2">
+        <button type="button" class="btn-evolution-outline-purple btn-evolution-text px-md-5">ACESSAR</button>
+      </div>
+      <div class="mt-3 mt-md-0">
+        <button type="button" class="btn-evolution-teal btn-evolution-text px-md-5" data-bs-toggle="modal" data-bs-target="#exampleModal">CONCLUÍDO</button>
+      </div>
+    </div>
+  </div>
+  `).join("");
+  contentBox.innerHTML += contentBoxTemplate;
+
+  const categoryNameTemplate = categories.map((item) => `
+    <p class="subtitle-dark">Teste${item}</p>
+  `).join("");
+  categoryName.innerHTML += categoryNameTemplate;
 };
 
 addItemIntoRoadmap();
